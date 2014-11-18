769df1ae15d9d718c3f56003e727ce5966289cb6
Victor Perrin <victor.perrin8@gmail.com>
2014-11-18 18:55:27 +0100

    New code to show the map (better)

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ RoadShare
 ===
 Mobile application project
 
+Follow this link to make the project work (except Proguard)
+http://developer.android.com/google/play-services/setup.html
+
--- src/es/upm/etsiinf/dam/roadshare/MainActivity.java
+++ src/es/upm/etsiinf/dam/roadshare/MainActivity.java
@@ -1,16 +1,35 @@
 package es.upm.etsiinf.dam.roadshare;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	
+	private GoogleMap mMap;
+	
+	private void setUpMapIfNeeded() {
+	    // Do a null check to confirm that we have not already instantiated the map.
+	    if (mMap == null) {
+	        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+	                            .getMap();
+	        // Check if we were successful in obtaining the map.
+	        if (mMap != null) {
+	            // The Map is verified. It is now safe to manipulate the map.
+
+	        }
+	    }
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setUpMapIfNeeded();
 	}
 
 	@Override
